9f36a835a096c22c974fa48e4999e2a16d5a0911
tuankiet <tuankiet0303@gmail.com>
2023-11-22 13:58:21 +0700

    adding opcode for the vector architecture

--- TOP_CPU.v
+++ TOP_CPU.v
@@ -25,6 +25,23 @@ module RSICV_CPU
     wire [0:0] REG_write_enable;
     wire [31:0]REG_write_value, REG_rs1_data, REG_rs1_data;
 
+    
+    wire [`VLEN * 32 - 1 : 0] new_v_regs ;
+    wire [31:0] new_vl;
+    wire [31:0] new_vstart;
+    wire        new_vill;
+    wire [2:0]  new_vsew;
+    wire [2:0]  new_vlmul;
+
+    wire [`VLEN * 32 - 1 : 0] v_regs; 
+    wire [31 : 0]             vl; //hold the number of elements to be updated -> index the last element
+    wire [31 : 0]             vstart; //index the first element
+    wire                      vill; // illigal if attempt to set invalid value to vetype
+    wire [2:0]                vsew;
+    wire [2:0]                vlmul;
+    wire [31:0]               element_width;
+
+
     always @(posedge SYS_clk)
     begin
         if (SYS_reset)
@@ -68,6 +85,29 @@ module RSICV_CPU
         .REG_rs2_data       (REG_rs2_data)
     );
 
+    
+    VECTOR_REGISTER_FILE VECTOR_REGISTER_FILE
+    (
+        .SYS_reset      (SYS_reset), 
+        .SYS_clk        (SYS_clk),
+        //INPUT, i.e. next value
+        .new_v_regs     (new_v_regs) ,
+        .new_vl         (new_vl),
+        .new_vstart     (new_vstart),
+        .new_vill       (new_vill),
+        .new_vsew       (new_vsew),
+        .new_vlmul      (new_vlmul),
+
+        //OUTPUT
+        .v_regs         (v_regs), //cannot, we can't assign to all, mux index by address
+        .vl             (vl), //hold the number of elements to be updated -> index the last element
+        .vstart         (vstart), //index the first element
+        .vill           (vill), // illigal if attempt to set invalid value to vetype
+        .vsew           (vsew),
+        .vlmul          (vlmul),
+        .element_width  (element_width)
+    );
+
     DATA_PATH DATA_PATH
     (
         //INPUT
@@ -76,6 +116,13 @@ module RSICV_CPU
         .REG_rs2_data       (REG_rs2_data),
         .MEM_read_data      (MEM_read_data),
         .PC                 (PC),
+        .v_regs(v_regs),
+        .vl(vl),
+        .vstart(vstart),
+        .vill(vill),
+        .vsew(vsew),
+        .vlmul(vlmul),
+        .element_width(element_width),
 
         //OUTPUT
         .new_PC             (new_PC),
@@ -91,45 +138,15 @@ module RSICV_CPU
         .invalid_instruction(invalid_instruction),
 
         .rs1                (rs1),
-        .rs2                (rs2)
-    );
+        .rs2                (rs2),
 
-    wire [`VLEN * 32 - 1 : 0] v_regs;
-    wire  [`VLEN * 32 - 1 : 0] new_v_regs;
-
-    VECTOR_REGISTER_FILE VECTOR_REGISTER_FILE
-    (
         .new_v_regs(new_v_regs),
-        .v_regs(v_regs)
-    );
-    
-    reg   [(`VLEN - 1) : 0]    new_vector_register [0 : 31];
-    wire  [(`VLEN - 1) : 0]    vector_register     [0 : 31];
-    
-    genvar i;
-    generate
-        for (i = 0; i <= 31; i = i + 1)
-        begin: vector_assign
-            assign vector_register[i] = v_regs [(i * `VLEN) + (`VLEN - 1) : (i * `VLEN)];
-            assign new_v_regs[(i * `VLEN) + (`VLEN - 1) : (i * `VLEN)] = new_vector_register[i];
-        end
-    endgenerate
-
-    /*
-    reg [(`VLEN - 1) : 0] vector_register [0 : 31];
-    always @(v_regs) 
-    begin
-        for (i = 0; i <= 31; i = i + 1)
-            
-    end
-
-    always @(new_vector_register)
-    // always @(*)
-    begin
-        for (i = 0; i <= 31; i = i + 1)
-            new_v_regs[(i * `VLEN) + (`VLEN - 1) : (i * `VLEN)] = new_vector_register[i];
-    end
-    */
+        .new_vl(new_vl),
+        .new_vstart(new_vstart),
+        .new_vill(new_vill),
+        .new_vsew(new_vsew),
+        .new_vlmul(new_vlmul),
+    );    
 endmodule;
 
 
@@ -141,6 +158,15 @@ module DATA_PATH
     input [31:0] MEM_read_data,
     input [31:0] PC,
 
+    //vector update
+    input [`VLEN * 32 - 1 : 0] v_regs; 
+    input [31 : 0]             vl; //hold the number of elements to be updated -> index the last element
+    input [31 : 0]             vstart; //index the first element
+    input                      vill; // illigal if attempt to set invalid value to vetype
+    input [2:0]                vsew;
+    input [2:0]                vlmul;
+    input [31:0]               element_width;
+ 
 
     output reg [31:0] new_PC,
     output reg [31:0] REG_write_value,
@@ -154,8 +180,17 @@ module DATA_PATH
     output reg [31:0] MEM_read_address,
     output reg [0:0]  invalid_instruction,
     output wire [4:0] rs1,
-    output wire [4:0] rs2
+    output wire [4:0] rs2,
+
+    //vector update
+    output wire [`VLEN * 32 - 1 : 0] new_v_regs ;
+    output reg [31:0] new_vl;
+    output reg [31:0] new_vstart;
+    output reg        new_vill;
+    output reg [2:0]  new_vsew;
+    output reg [2:0]  new_vlmul;
 );
+
     wire [6:0] opcode   = instruction [6:0];
     wire [4:0] rd       = instruction [11:7];
     assign     rs1      = instruction [19:15];
@@ -168,10 +203,39 @@ module DATA_PATH
     wire [12:1]B_immed  ={instruction[31], instruction[7], instruction[30:25], instruction[11:8]}; //B-type only, for conditional BRANCH
     wire [20:1]J_immed  ={instruction[31], instruction[19:12], instruction[20], instruction[30:21]};//J-type only, for JUMP-AND-LINK
     wire[31:12]U_immed  = instruction[31:12]; //U_type only, for LUI, wide immediate instruction....
+    //vector update
+    //vector load
+    wire [2:0] width    = instruction[14:12];
+    wire       vm       = instruction [25];
+    wire [1:0] mop      = instruction [27:26];
+    wire       mew      = instruction[28];
+    wire [2:0] nf       = instruction [31:29] ;
+    wire [4:0] vd       = instruction [11:7];
+    wire [4:0] vs3      = instruction [11:7];
+    // vector arthimetic
+    wire [5:0] funct6   = instruction [31:26];
+    wire [4:0] vs2      = instruction [24:20];
+    wire [4:0] vs1      = instruction [19:15];
+    //vector configuration instructions
+    wire [10:0]zimm11   = instruction [30:20];
+    wire [4:0] uimm     = instruction [19:15];
 
     reg branch_taken;
 
-    always @(instruction, REG_rs1_data, REG_rs2_data, MEM_read_data, PC) // all the input
+    reg   [(`VLEN - 1) : 0]    new_vector_register [0 : 31];
+    wire  [(`VLEN - 1) : 0]    vector_register     [0 : 31];
+
+    genvar i;
+    generate
+        for (i = 0; i <= 31; i = i + 1)
+        begin: vector_assign
+            assign vector_register[i]                                  = v_regs [(i * `VLEN) + (`VLEN - 1) : (i * `VLEN)];
+            assign new_v_regs[(i * `VLEN) + (`VLEN - 1) : (i * `VLEN)] = new_vector_register[i];
+        end
+    endgenerate
+
+    integer k;
+    always @(instruction, REG_rs1_data, REG_rs2_data, MEM_read_data, PC, v_regs, vl, vstart, vill, vsew, vsew, vlmul, element_width) // all the input
     begin
         //set the default, also prevent latch
         new_PC              = PC + 4;
@@ -185,6 +249,17 @@ module DATA_PATH
 
         branch_taken        = 0;
         invalid_instruction = 0;
+
+        for (k = 0; k <= 31; k = k+1)
+        begin
+            new_vector_register[k] = vector_register[k];
+        end
+        
+        new_vl              = vl;
+        new_vstart          = vstart;
+        new_vill            = vill;
+        new_vsew            = vsew;
+        vlmul               = new_vlmul;
 
         case (opcode)
             7'b0110011: 
@@ -274,6 +349,56 @@ module DATA_PATH
                 endcase
 
                 if (branch_taken) new_PC = PC + $signed(B_immed << 1);
+            end
+
+            7'b0000111: //vector load
+            begin
+              
+            end
+
+            7'b0100111: //vector store
+            begin
+            
+            end
+
+            7'b1010111: //vector arthimetic
+            begin
+                case (funct3)
+                    3'b010: //OPMVV vector and vector multiply
+
+                    3'b011: //OPIVI vector and IMM
+
+                    3'b100: //OPIVX vector and scalar I base
+
+                    3'b110: //OPMVX vector and scalar
+
+                    3'b000: //OPIVV  vector and vector I base
+
+                    default: invalid_instruction = 1;
+                endcase
+            end
+            7'b1010111: //vector configuration
+            begin
+                if (instruction[2:0] == 3'b111)
+                begin
+                    if (instruction[31] == 0)   //vsetvli
+                    begin
+                        
+                    end
+                    
+                    else if (instruction [30] == 1)//vsetivli
+                    begin
+                        
+                    end
+                    
+                    else if (instruction[30:25] == 0) //vsetvl
+                    begin
+                      
+                    end
+
+                    else invalid_instruction = 1;
+                end
+                else invalid_instruction = 1;
             end
 
             default: invalid_instruction = 1;
