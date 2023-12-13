49947125ee2f2a643cd70da54e51e9d4b79feec6
tuankiet <tuankiet0303@gmail.com>
2023-12-13 20:35:38 +0700

    pass logic test

--- test/10.s
+++ test/10.s
@@ -1,15 +1,11 @@
-addi x1, zero, 1
-addi x2, zero, 2
-addi x3, zero, 3
-li x4, 4
-li x5, 5
-li x6, 0xFFFFFFFF
-sra x6, x6, x5
-sra x6, x6, x6
-srai x6, x6, 3
-sll x6, x6, x3
-slli x6, x6, 2
-srl x6, x6, x2
-srli x6, x6, 10
-#test all shift
-# result in x5
+li x6, 0xF0FF00F0
+andi x6, x6, -1
+ori x6, x6, -1
+xori x6, x6, -1
+li x6, 0xF0FF00F0
+li x5, 0xFFFFFFFF
+and x6, x6, x5
+or x6, x6, x5
+xor x6, x6, x6
+#test all logical
+# result in x6
