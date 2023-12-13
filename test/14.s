f5838bfdae3c5fac37ae3b190cbae3567de64056
tuankiet <tuankiet0303@gmail.com>
2023-12-13 22:04:54 +0700

    pass multi and dvide test

---
+++ test/14.s
@@ -0,0 +1,13 @@
+addi x1, zero, -2
+addi x2, zero, -5
+mul x3, x1, x2
+mulh x3, x1, x2
+mulhu x3, x1, x2
+mulhsu x3, x1, x2
+mulhsu x3, x2, x1
+
+addi x1, zero, 14
+div x3, x1, x2
+divu x3, x2, x1
+rem x3, x1, x2
+remu x3, x2, x1
