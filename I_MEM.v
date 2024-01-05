29149b5e58013c4a7e3ce012f07fe28bd186b35a
tuankiet <tuankiet0303@gmail.com>
2024-01-05 20:18:29 +0700

    sửa để có thể nhận từ buffer

--- I_MEM.v
+++ I_MEM.v
@@ -5,17 +5,21 @@ module INS_MEMORY
     input wire [0:0] SYS_reset,
     input  wire [31:0]  PC,
 
+    input      PC_to_mem_enable,
+    input[7:0] PC_to_mem_data,  
+    input[31:0]PC_to_mem_address,
+
     output wire [31:0]  instruction
 );
-    reg [31:0] data [(`INS_START_ADDRESS >> 2) : (`INS_START_ADDRESS >> 2) + 1000];
-    assign instruction[31:0] = data[PC >> 2];
+    reg [7:0] data [(`INS_START_ADDRESS) : (`INS_START_ADDRESS) + 1000];
+    assign instruction[31:0] = {data[PC], data[PC+1], data[PC+2], data[PC+3]};
 
     integer i, file;
     always @(posedge SYS_clk) 
     begin
         if (SYS_reset)
         begin
-            for(i=(`INS_START_ADDRESS >> 2); i<(`INS_START_ADDRESS >> 2) + 1000 ;i=i+1)
+            for(i=`INS_START_ADDRESS; i<`INS_START_ADDRESS + 1000 ;i=i+1)
             begin
                 data[i] = 0;
             end
@@ -23,6 +27,14 @@ module INS_MEMORY
             `ifdef TESTING
             $readmemh("C:/Users/tuankiet/Desktop/RISC-V/test/input_text.txt", data);
             `endif
+        end
+
+        else
+        begin
+            if (PC_to_mem_enable)
+            begin
+                data[PC_to_mem_address] <= PC_to_mem_data;
+            end     
         end
     end
 
